e80ce750e6fce2c0c031b81b47443ecb0f85ba5b
ms <alstjq1012@gmail.com>
2025-07-10 02:51:55 +0900

    add song viewmodels

---
+++ lib/features/daily-song/viewmodels/song_viewmodel.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hidi/features/daily-song/models/song_model.dart';
+import 'package:hidi/features/daily-song/repos/song_repo.dart';
+
+class SongViewmodel extends AsyncNotifier<Song> {
+  late final SongRepository _songRepo;
+  @override
+  FutureOr<Song> build() {
+    // TODO: implement build
+    _songRepo = ref.read(songRepo);
+    throw UnimplementedError();
+  }
+
+  Future<void> getSongById(int id) async {
+    state = AsyncValue.loading();
+    final song = await _songRepo.getSongById(id);
+    if (song != null) {
+      state = AsyncValue.data(song);
+    } else {
+      state = AsyncValue.data(Song.empty());
+    }
+  }
+
+  Future<void> getSongByDate(String date) async {
+    state = AsyncValue.loading();
+    final song = await _songRepo.getSongByDate(date);
+    if (song != null) {
+      state = AsyncValue.data(song);
+    } else {
+      state = AsyncValue.data(Song.empty());
+    }
+  }
+}
