832302f93929bb27c7fc86b275a825cad6d458d4
ms <alstjq1012@gmail.com>
2025-07-09 22:43:22 +0900

    initialize mypage ui & edit & setting page

--- lib/features/users/views/user_profile_view.dart
+++ lib/features/users/views/user_profile_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:hidi/constants/sizes.dart';
-import 'package:hidi/features/authentication/repos/authentication_repo.dart';
-import 'package:hidi/features/authentication/viewmodels/login_view_model.dart';
+import 'package:hidi/features/settings/views/settings_view.dart';
 import 'package:hidi/features/users/viewmodels/user_profile_view_model.dart';
+import 'package:hidi/features/users/views/user_profile_edit_view.dart';
 
 class MyPageView extends ConsumerStatefulWidget {
   const MyPageView({super.key});
@@ -14,12 +12,6 @@ class MyPageView extends ConsumerStatefulWidget {
 }
 
 class _MyPageViewState extends ConsumerState<MyPageView> {
-  late final TextEditingController _nicknameController =
-      TextEditingController();
-  late final TextEditingController _passwordController =
-      TextEditingController();
-  Map<String, dynamic> formData = {};
-
   final String userName = 'Hidi';
   final int followers = 1337;
   final int following = 42;
@@ -27,32 +19,43 @@ class _MyPageViewState extends ConsumerState<MyPageView> {
 
   final List<Map<String, dynamic>> playlists = [
     {'name': 'Liked Songs', 'count': 128, 'icon': Icons.favorite},
-    {'name': 'Liked Songs', 'count': 128, 'icon': Icons.favorite},
-    {'name': 'Liked Songs', 'count': 128, 'icon': Icons.favorite},
-    {'name': 'Liked Songs', 'count': 128, 'icon': Icons.favorite},
-    {'name': 'Liked Songs', 'count': 128, 'icon': Icons.favorite},
   ];
+  final ScrollController _scrollController = ScrollController();
+
+  double _appBarOpacity = 0.0;
   @override
   void initState() {
     super.initState();
+    _scrollController.addListener(() {
+      final maxScroll = 200.0; // The scroll distance to reach full opacity
+      final offset = _scrollController.offset;
+      final newOpacity = (offset / maxScroll).clamp(0.0, 1.0);
+      if (newOpacity != _appBarOpacity) {
+        setState(() {
+          _appBarOpacity = newOpacity;
+        });
+      }
+    });
   }
 
-  void signOut(int id) async {
-    await ref.read(loginProvider.notifier).signOut(context, id);
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
-  void reIssue() async {
-    await ref.read(authRepo).postReissue();
+  void _OnEditProfile() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => UserProfileEditView()),
+    );
   }
 
-  void _onClearTap(TextEditingController controller) {
-    controller.clear();
-  }
-
-  void _onSubmit() async {
-    await ref
-        .read(userProfileProvider.notifier)
-        .patchCurrentUser(formData["nickname"], formData["password"]);
+  void _OnSettings() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SettingsView()),
+    );
   }
 
   void deleteUser() async {
@@ -61,18 +64,155 @@ class _MyPageViewState extends ConsumerState<MyPageView> {
 
   @override
   Widget build(BuildContext context) {
-    // final userState = ref.watch(userProfileProvider);
     return Scaffold(
-      body: SafeArea(
-        child: CustomScrollView(
-          physics: BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics(),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: CustomScrollView(
+              controller: _scrollController,
+              physics: const AlwaysScrollableScrollPhysics(),
+              slivers: [
+                _buildUserInfo(),
+                _buildSectionHeader('Public Playlists'),
+                _buildPlaylists(),
+                SliverFillRemaining(),
+              ],
+            ),
           ),
-          slivers: [
-            _buildSliverAppBar(),
-            _buildUserInfo(),
-            _buildSectionHeader('Public Playlists'),
-            _buildPlaylists(),
+          _buildAppBar(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.teal.shade300.withValues(alpha: _appBarOpacity),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings_outlined),
+            onPressed: _OnSettings,
+          ),
+        ],
+
+        title: Text(
+          userName,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: Colors.white.withValues(alpha: _appBarOpacity),
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildUserInfo() {
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.teal.shade800, Colors.white],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 30),
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Colors.teal,
+                    child: Text(
+                      userName.substring(0, 1),
+                      style: const TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      _buildStatColumn('Followers', followers),
+                      _buildStatColumn('Following', following),
+                      OutlinedButton(
+                        onPressed: _OnEditProfile,
+                        style: OutlinedButton.styleFrom(
+                          foregroundColor: Colors.black,
+                          side: const BorderSide(color: Colors.white54),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        child: const Text('Edit Profile'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStatColumn(String label, int number) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(number.toString(), style: Theme.of(context).textTheme.titleLarge),
+        const SizedBox(height: 4.0),
+        Text(label, style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
+  }
+
+  SliverToBoxAdapter _buildSectionHeader(String title) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  SliverList _buildPlaylists() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        final playlist = playlists[index];
+        return ListTile(
+          leading: Icon(playlist['icon'], size: 30, color: Colors.greenAccent),
+          title: Text(
+            playlist['name'],
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          subtitle: Text(
+            '${playlist['count']} songs',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          trailing: const Icon(Icons.chevron_right),
+          onTap: () {},
+        );
+      }, childCount: playlists.length),
+    );
+  }
+}
+
+
             // SliverToBoxAdapter(
             //   child: TextButton(
             //     child: Text("SignOut"),
@@ -169,122 +309,3 @@ class _MyPageViewState extends ConsumerState<MyPageView> {
             //     ],
             //   ),
             // ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  SliverAppBar _buildSliverAppBar() {
-    return SliverAppBar(
-      expandedHeight: 250.0,
-      elevation: 0,
-      actions: [
-        IconButton(icon: const Icon(Icons.settings_outlined), onPressed: () {}),
-      ],
-      flexibleSpace: FlexibleSpaceBar(
-        centerTitle: true,
-        title: Text(
-          userName,
-          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-        background: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.teal.shade800, Colors.black],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 30),
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundColor: Colors.teal,
-                  child: Text(
-                    userName.substring(0, 1),
-                    style: const TextStyle(fontSize: 40, color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  SliverToBoxAdapter _buildUserInfo() {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            _buildStatColumn('Followers', followers),
-            _buildStatColumn('Following', following),
-            OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.white,
-                side: const BorderSide(color: Colors.white54),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-              child: const Text('Edit Profile'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildStatColumn(String label, int number) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(number.toString(), style: Theme.of(context).textTheme.titleLarge),
-        const SizedBox(height: 4.0),
-        Text(label, style: Theme.of(context).textTheme.bodyMedium),
-      ],
-    );
-  }
-
-  SliverToBoxAdapter _buildSectionHeader(String title) {
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: Text(
-          title,
-          style: Theme.of(context).textTheme.titleLarge?.copyWith(fontSize: 20),
-        ),
-      ),
-    );
-  }
-
-  SliverList _buildPlaylists() {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-        final playlist = playlists[index];
-        return ListTile(
-          leading: Icon(playlist['icon'], size: 30, color: Colors.greenAccent),
-          title: Text(
-            playlist['name'],
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          subtitle: Text(
-            '${playlist['count']} songs',
-            style: Theme.of(context).textTheme.bodyMedium,
-          ),
-          trailing: const Icon(Icons.chevron_right),
-          onTap: () {},
-        );
-      }, childCount: playlists.length),
-    );
-  }
-}
